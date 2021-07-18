f1c078e64c6a3e869f56c1f24c638d45122fd014
jenkins <fadi1707@outlook.com>
2021-07-18 14:14:33 +0300

    update organize.sh

--- organize.sh
+++ organize.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 IMG_NAME=$1
+WRKSPC="1"
 CURRENT_DIR=`pwd`
-NEW_DIR="${CURRENT_DIR}@2"
 
-cp $IMG_NAME $NEW_DIR/deployment
+echo $CURRENT_DIR | grep -o "@" 
+
+if [ $? -eq 0 ]
+then 
+    WRKSPC=`echo $CURRENT_DIR | cut -d'@' -f2`
+    CURRENT_DIR=`echo $CURRENT_DIR | cut -d'@' -f1`
+    echo $CURRENT_DIR
+fi
+
+WRKSPC=$((WRKSPC+1))
+
+NEW_DIR="${CURRENT_DIR}@${WRKSPC}"
+echo $NEW_DIR
+echo "cp $CURRENT_DIR/$IMG_NAME $NEW_DIR/deployment"
